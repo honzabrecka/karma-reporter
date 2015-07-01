acddbb1b0388d47f1f4078e392561262699d379e
Jan Brecka <jan.brecka@gmail.com>
2015-07-01 17:49:43 +0200

    do not depend on itself

--- project.clj
+++ project.clj
@@ -5,10 +5,11 @@
             :url "http://www.opensource.org/licenses/mit-license.php"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "0.0-3308"]
-                 [karma-reporter "0.1.0-SNAPSHOT"]]
+                 ;[karma-reporter "0.1.0-SNAPSHOT"]
+                ]
   :plugins [[lein-cljsbuild "1.0.6"]]
   :cljsbuild {:builds [{:id "test"
-                        :source-paths ["example_src"]
+                        :source-paths ["example_src" "src"]
                         :compiler {:output-to "target/public/test/foo.js"
                                    :output-dir "target/public/test"
                                    :main foo.test-runner
