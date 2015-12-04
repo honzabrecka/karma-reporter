73ecc2a4be50be1142f8aca2310839e1e72f630b
Jan Brecka <jan.brecka@gmail.com>
2015-12-04 16:43:19 +0100

    change args order (actual is first now)

--- example_src/foo/bar_test.cljs
+++ example_src/foo/bar_test.cljs
@@ -2,9 +2,9 @@
   (:require [cljs.test :refer-macros [deftest is testing run-tests]]))
 
 (deftest add-test
-  (is (= 5 (+ 2 3))))
+  (is (= (+ 2 3) 5)))
 
 (deftest subtract-test
-  (is (= 3 (- 7 4))))
+  (is (= (- 7 4) 3)))
 
 (defn not-a-test [])
