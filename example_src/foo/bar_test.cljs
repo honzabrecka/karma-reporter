aaa0496af27a6fa8d985e0e8a338d2b048055d94
Imre Koszo <imrekoszo@users.noreply.github.com>
2015-12-11 11:04:32 +0000

    adding message support to format-log

--- example_src/foo/bar_test.cljs
+++ example_src/foo/bar_test.cljs
@@ -7,4 +7,9 @@
 (deftest subtract-test
   (is (= (- 7 4) 3)))
 
+;; Uncomment to see how failures are logged
+#_(deftest failing-test
+  (is (= 1 3))
+  (is (= 1 2) "One should be equal to two"))
+
 (defn not-a-test [])
