b2c758d8c3607c193042b237552875f492c5cdea
Imre Koszo <imrekoszo@users.noreply.github.com>
2015-12-14 11:33:02 +0000

    improving test failure message display

--- example_src/foo/bar_test.cljs
+++ example_src/foo/bar_test.cljs
@@ -10,6 +10,7 @@
 ;; Uncomment to see how failures are logged
 #_(deftest failing-test
   (is (= 1 3))
-  (is (= 1 2) "One should be equal to two"))
+  (is (= 1 2) "One should be equal to two")
+  (is (= 3 4) (str "Let me add in something\nfancy here: " {:a 1234})))
 
 (defn not-a-test [])
