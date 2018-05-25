bfbc209eac81d1dc91423761c11e015b40bbc789
Juho Teperi <juho.teperi@iki.fi>
2018-05-25 16:57:32 +0300

    Update fipp
    Includes fix for https://github.com/brandonbloom/fipp/issues/44

--- project.clj
+++ project.clj
@@ -9,7 +9,7 @@
 
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.9.229"]
-                 [fipp "0.6.7"]]
+                 [fipp "0.6.12"]]
 
   :plugins [[lein-cljsbuild "1.1.1"]]
 
